20d1b05ab959e3f77c3f77ba1b5e2e0b410e49ff
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-04-11 01:25:18 +0530

    Updated Icon components

--- src/components/icons/HomeIcon/index.js
+++ src/components/icons/HomeIcon/index.js
@@ -1,28 +1,21 @@
 import * as React from 'react';
-import Svg, {Path} from 'react-native-svg';
+import Svg, {Path, G} from 'react-native-svg';
 
 function ProfileIcon(props) {
+  console.log(props.isFocused);
   return (
-    <Svg
-      width={24}
-      height={24}
-      viewBox="0 0 24 24"
-      fill={props.isFocused ? '#67B521' : 'none'}
-      {...props}>
-      <Path
-        d="M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
-        stroke="#000"
+    <Svg width={24} height={24} viewBox="0 0 24 24" {...props}>
+      <G
+        stroke={props.isFocused ? '#67B521' : 'black'}
+        fill={props.isFocused ? '#67B521' : 'none'}
+        fillRule="evenodd"
         strokeWidth={2}
         strokeLinecap="round"
         strokeLinejoin="round"
-      />
-      <Path
-        d="M9 22V12h6v10"
-        stroke="#000"
-        strokeWidth={2}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+        transform="translate(1 1)">
+        <Path d="M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
+        <Path d="M9 22V12h6v10" stroke={props.isFocused ? 'white' : 'black'} />
+      </G>
     </Svg>
   );
 }
--- src/components/icons/ProfileIcon/index.js
+++ src/components/icons/ProfileIcon/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Svg, {Path} from 'react-native-svg';
+import Svg, {Path, G} from 'react-native-svg';
 
 function HomeIcon(props) {
   return (
@@ -9,13 +9,18 @@ function HomeIcon(props) {
       viewBox="0 0 24 24"
       fill={props.isFocused ? '#67B521' : 'none'}
       {...props}>
-      <Path
-        d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2M12 11a4 4 0 100-8 4 4 0 000 8z"
-        stroke="#000"
+      <G
+        stroke={props.isFocused ? '#67B521' : 'black'}
+        fill={props.isFocused ? '#67B521' : 'none'}
+        fillRule="evenodd"
         strokeWidth={2}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+        transform="translate(1 1)">
+        <Path
+          d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2M12 11a4 4 0 100-8 4 4 0 000 8z"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </G>
     </Svg>
   );
 }
