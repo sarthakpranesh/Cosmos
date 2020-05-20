98d9dc65b5ac09f0f7877cfd8f8810b0d69efa11
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-05-20 14:59:55 +0530

    added travis build and upload script

---
+++ upload.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  mkdir $HOME/buildApk/
+  mkdir $HOME/Github/
+
+  cp -R android/app/build/outputs/apk/release/*.apk $HOME/buildApk/
+
+  cd $HOME/Github/
+  git config --global user.email "sarthak.pranesh2018@vitstudent.ac.in" 
+  git config --global user.name "sarthakpranesh" 
+
+  mkdir cosmos
+  cd cosmos
+  git init
+  git remote add origin https://sarthakpranesh:$GITHUB_API_KEY@github.com/sarthakpranesh/cosmos.ReactNative.git
+  git checkout -b build
+  cp -Rf $HOME/buildApk/* .
+  git add .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER push" 
+  git push origin build -f
+  echo "Done"
+fi
