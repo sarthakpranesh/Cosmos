a814b3bbe438431c0aff3e6c44541f94464e1b71
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-06-17 19:50:10 +0530

    Removed unused packages and updated travis script for build uploads
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    'module:react-native-dotenv',
-  ],
+  presets: ['module:metro-react-native-babel-preset'],
   env: {
     production: {
       plugins: ['react-native-paper/babel'],
