a5934dd4f48780ce9071c82856f2fe2934a12251
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-07-12 19:24:38 +0530

    Updated .travis.yml and added react-native-notification package
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- android/app/src/main/java/com/cosmos/MainApplication.java
+++ android/app/src/main/java/com/cosmos/MainApplication.java
@@ -13,6 +13,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.wix.reactnativenotifications.RNNotificationsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
