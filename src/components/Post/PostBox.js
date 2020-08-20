14f5d009465e792f5955d829f8215095144cf01b
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-08-21 01:46:06 +0530

    Using react-native-tap
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- src/components/Post/PostBox.js
+++ src/components/Post/PostBox.js
@@ -1,7 +1,7 @@
-import React, {useState, useRef} from 'react';
+import React, {useState} from 'react';
 import {View, Dimensions, StyleSheet} from 'react-native';
 import Svg, {Path} from 'react-native-svg';
-import {TapGestureHandler, State} from 'react-native-gesture-handler';
+import {MultiTap} from 'react-native-tap';
 
 const {width} = Dimensions.get('window');
 
@@ -10,51 +10,35 @@ const PostBox = ({
   onSingleTap = () => {},
   onDoubleTap = () => {},
 }) => {
-  let doubleTapRef = useRef();
   const [showHeart, setShowHeart] = useState(false);
 
-  const _onSingleTap = (event) => {
-    if (event.nativeEvent.state === State.ACTIVE) {
-      onSingleTap();
-    }
-  };
-
-  const _onDoubleTap = (event) => {
-    if (event.nativeEvent.state === State.ACTIVE) {
-      setShowHeart(true);
-      onDoubleTap();
-      setTimeout(() => {
-        setShowHeart(false);
-      }, 1000);
-    }
-  };
-
   return (
-    <TapGestureHandler
-      onHandlerStateChange={_onSingleTap}
-      waitFor={doubleTapRef}>
-      <TapGestureHandler
-        ref={doubleTapRef}
-        onHandlerStateChange={_onDoubleTap}
-        numberOfTaps={2}>
-        <View>
-          {children}
-          <View style={[styles.Overlay]}>
-            <Svg
-              width={width / 2}
-              height={width / 2}
-              viewBox="0 0 24 24"
-              fill={showHeart ? 'rgba(255, 255, 255, 0.6)' : 'none'}>
-              <Path
-                d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.501 5.501 0 10-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78v0z"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </Svg>
-          </View>
+    <MultiTap
+      onSingleTap={onSingleTap}
+      onDoubleTap={() => {
+        setShowHeart(true);
+        onDoubleTap();
+        setTimeout(() => {
+          setShowHeart(false);
+        }, 1000);
+      }}>
+      <View>
+        {children}
+        <View style={[styles.Overlay]}>
+          <Svg
+            width={width / 2}
+            height={width / 2}
+            viewBox="0 0 24 24"
+            fill={showHeart ? 'rgba(255, 255, 255, 0.6)' : 'none'}>
+            <Path
+              d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.501 5.501 0 10-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78v0z"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </Svg>
         </View>
-      </TapGestureHandler>
-    </TapGestureHandler>
+      </View>
+    </MultiTap>
   );
 };
 
